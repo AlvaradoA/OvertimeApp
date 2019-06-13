f0e4ff42cdcb71825c322340d459bef507dac8f7
Aldo <odla12345@yahoo.com>
2019-06-13 13:27:04 +0800

    Added posts functionalities

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,28 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    end
+  end
+
+  def show
+    set_post
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:date, :rationale)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
