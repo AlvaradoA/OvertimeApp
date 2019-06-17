ae3724cec4afa320cb960317f08b4d6923caa523
Aldo <odla12345@yahoo.com>
2019-06-17 11:22:02 +0800

    Implemented tests and page redirec for permission structure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
 
   def update
     authorize @post
-    
+
     if @post.update(post_params)
       redirect_to @post, notice: 'Your post was edited successfully'
     else
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before do
-    @user = FactoryGirl.build_stubbed(:user)
+    @user = FactoryGirl.create(:user)
   end
   describe "creation" do
 
