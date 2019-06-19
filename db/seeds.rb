238ee39b962ce29b365ee8bfb48ef7d1820bbb3a
Aldo <odla12345@yahoo.com>
2019-06-19 15:56:57 +0800

    Added live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ puts "1 admin user created"
 puts "1 user created"
 
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create(date: Date.today, rationale: "#{post} Lorem ipsum dolor sit amet, id aenean, a facilisis integer nullam ut pede, id suspendisse eget sit viverra lectus condimentum, non pellentesque praesent tincidunt mollit. Erat bibendum nibh, porta ut eget praesent integer fringilla, at et facilisis sem ut dis ac, et tristique lacinia nec maecenas curabitur, facilisis nam lobortis nunc ultricies sagittis. Scelerisque sit, in nam tempor, sodales sit, eu volutpat eget lectus, faucibus suscipit diam. Venenatis scelerisque leo ad auctor volutpat eros, fringilla pede primis amet tristique, pede id eu class sit magna mi. Scelerisque auctor convallis arcu sodales in orci. Dis orci. At lacus enim potenti nunc vestibulum, commodo est, massa ullamcorper vivamus malesuada, fames fermentum in nec a, euismod leo neque sed eros. In gravida porta vitae nec eu. Felis cras leo, cursus lorem cursus sed felis integer, eu corrupti aliquam consectetuer dolorem vestibulum, mauris quam. Ornare turpis ornare sit, erat ultrices et curabitur tellus lacus, sit ante. Sit lectus nostra rutrum ut eget, luctus quis, turpis erat et, eu et justo sodales id. Aliquam aliquam tortor scelerisque, tortor fusce, ipsum semper mauris urna sapien nostra sagittis, mus eu donec interdum et, ac nostra pellentesque maecenas nam imperdiet donec.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
