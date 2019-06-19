25601d9963b8ca41539b368ab3a1477c680c4b43
Aldo <odla12345@yahoo.com>
2019-06-19 10:33:51 +0800

    Implemented phone validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-@user = User.create(email: "email@email.com", password: "password", password_confirmation: "password", first_name: "fname", last_name: "lname", phone: "432231242")
+@user = User.create(email: "email@email.com", password: "password", password_confirmation: "password", first_name: "fname", last_name: "lname", phone: "4322312421")
 
 puts "1 user created"
 
-AdminUser.create(email: "admin@email.com", password: "password", password_confirmation: "password", first_name: "Admin", last_name: "User", phone: "432231242")
+AdminUser.create(email: "admin@email.com", password: "password", password_confirmation: "password", first_name: "Admin", last_name: "User", phone: "4322312421")
 
 puts "1 admin user created"
 
