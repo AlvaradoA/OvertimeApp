4fb2d91b7ea0f49a6fa22505d90eea587d3c3a61
Aldo <odla12345@yahoo.com>
2019-06-19 13:00:27 +0800

    Blocked non admin users from accessing audit log index page

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -18,6 +18,12 @@ describe 'AuditLog Features' do
       expect(page).to have_content(@audit_log.user.full_name)
     end
 
-    
+    it 'cannot be accessed by non admin users' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+      visit audit_logs_path
+      expect(current_path).to eq(root_path)
+    end
   end
 end
